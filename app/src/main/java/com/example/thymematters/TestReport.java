e9e759a42d127b3b133c94d4553dce5b71368388
RushilPatel0703 <rushilpatel0703@gmail.com>
2021-08-20 02:21:58 +0200

    Added a pie chart using Anycharts

--- app/src/main/java/com/example/thymematters/TestReport.java
+++ app/src/main/java/com/example/thymematters/TestReport.java
@@ -1,6 +1,7 @@
 package com.example.thymematters;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,7 +9,11 @@ import com.anychart.AnyChart;
 import com.anychart.AnyChartView;
 import com.anychart.chart.common.dataentry.DataEntry;
 import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.chart.common.listener.Event;
+import com.anychart.chart.common.listener.ListenersInterface;
 import com.anychart.charts.Pie;
+import com.anychart.enums.Align;
+import com.anychart.enums.LegendLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,16 +26,43 @@ public class TestReport extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_report);
 
+        AnyChartView anyChartView = findViewById(R.id.any_chart_view);
+
         Pie pie = AnyChart.pie();
 
+        pie.setOnClickListener(new ListenersInterface.OnClickListener(new String[]{"x", "value"}) {
+            @Override
+            public void onClick(Event event) {
+                Toast.makeText(TestReport.this, event.getData().get("x") + ":" + event.getData().get("value"), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        int num = 6371664;
+
         List<DataEntry> data = new ArrayList<>();
-        data.add(new ValueDataEntry("John", 10000));
-        data.add(new ValueDataEntry("Jake", 12000));
-        data.add(new ValueDataEntry("Peter", 18000));
+        data.add(new ValueDataEntry("Apples", num));
+        data.add(new ValueDataEntry("Pears", 789622));
+        data.add(new ValueDataEntry("Bananas", 7216301));
+        data.add(new ValueDataEntry("Grapes", 1486621));
+        data.add(new ValueDataEntry("Oranges", 1200000));
 
         pie.data(data);
 
-        AnyChartView anyChartView = (AnyChartView) findViewById(R.id.any_chart_view);
+        pie.title("Fruits imported in 2015 (in kg)");
+
+        pie.labels().position("outside");
+
+        pie.legend().title().enabled(true);
+        pie.legend().title()
+                .text("Retail channels")
+                .padding(0d, 0d, 10d, 0d);
+
+        pie.legend()
+                .position("center-bottom")
+                .itemsLayout(LegendLayout.HORIZONTAL)
+                .align(Align.CENTER);
+
         anyChartView.setChart(pie);
+
     }
 }
