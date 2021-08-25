a2890d00965e5cbf2020d184a3413ab6a65ee6e9
1864691 <69578327+1864691@users.noreply.github.com>
2021-08-25 15:38:07 +0200

    updated pages help feature fully designed

---
+++ app/src/main/java/com/example/thymematters/help_change_account_details.java
@@ -0,0 +1,14 @@
+package com.example.thymematters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class help_change_account_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_change_account_details);
+    }
+}
---
+++ app/src/main/java/com/example/thymematters/help_delivery_date.java
@@ -0,0 +1,50 @@
+package com.example.thymematters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class help_delivery_date extends AppCompatActivity {
+
+    Button new_date;
+    DatePickerDialog datePickerDialog;
+    int year;
+    int month;
+    int dayOfMonth;
+    Calendar calendar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_delivery_date);
+
+        TextView textView=findViewById(R.id.text);
+        new_date = findViewById(R.id.new_date);
+
+        new_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendar = Calendar.getInstance();
+                year = calendar.get(Calendar.YEAR);
+                month = calendar.get(Calendar.MONTH);
+                dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog = new DatePickerDialog(help_delivery_date.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        new_date.setText(year + "-" + (month + 1) + "-" + day);
+                    }
+                }, year, month, dayOfMonth);
+                //this ensures only future dates can be selected
+                //datePickerDialog.getDatePicker().setMinDate();;
+                datePickerDialog.show();
+            }
+        });
+    }
+}
